42d427b99c4b528bad7930f8d6a4b67eb2ef1a39
Sharooll <smezav1@unicartagena.edu.co>
2026-05-31 22:09:43 -0500

    Ejercicio 1.4: Cree una clase en donde utilice una de las interfaces

---
+++ Taller18/PersonalLimpieza.java
@@ -0,0 +1,7 @@
+public class PersonalLimpieza implements Limpieza {
+    
+    @Override
+    public void limpiar(){
+        System.out.println("El personal está realizando la limpieza ");
+    }
+}
